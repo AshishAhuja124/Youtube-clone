793c98abb6cd9fa5e097e486da39b753943c7f28
Ashish Ahuja <ashishahuja124@gmail.com>
2023-12-06 14:43:33 +0530

    created store and toggle menu

---
+++ src/components/MainContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ButtonList from './VideosSection/ButtonList'
+import VideosContainer from './VideosSection/VideosContainer'
+
+const MainContainer = () => {
+  return (
+    <div>
+        <ButtonList />
+        <VideosContainer />
+    </div>
+  )
+}
+
+export default MainContainer
