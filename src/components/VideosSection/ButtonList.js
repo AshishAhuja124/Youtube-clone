ca439cdbb6cda763f62279bd4ea4e50e9329b76b
Ashish Ahuja <ashishahuja124@gmail.com>
2023-12-06 16:41:03 +0530

    created videocard and integrated youtube api

--- src/components/VideosSection/ButtonList.js
+++ src/components/VideosSection/ButtonList.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Button from './Button'
+
+const list = ["All", "Gaming", "Music", "Cricket", "News", "Himalayas", "Trailers", "Tourism","React", "NextJs", "Computers", "Flights"]
 
 const ButtonList = () => {
   return (
-    <div>ButtonList</div>
-  )
+    <div className='flex overflow-x-hidden'>
+      <div className='flex no-scrollbar overflow-y-auto'>
+        {list.map((item) => <Button key={item} name={item} />)}
+      </div>
+    </div>
+  );
 }
 
 export default ButtonList
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+const GOOGLE_API_KEY = "AIzaSyAEUJ73Hj9UfOyizMklf3ATAA4B17wq9d4"
+
+export const YOUTUBE_VIDEOS_API = 
+"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+GOOGLE_API_KEY
+
