e786558b577fe017d4055af0c53a20f37f30d848
Ashish Ahuja <ashishahuja124@gmail.com>
2023-12-07 12:40:07 +0530

    Built watch page and showing videos

--- src/components/VideosSection/WatchPage.js
+++ src/components/VideosSection/WatchPage.js
@@ -1,16 +1,29 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../../store/appSlice';
+import { useSearchParams } from 'react-router-dom';
 
 const WatchPage = () => {
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
 
   useEffect(() => {
     dispatch(closeMenu());
   },[])
 
   return (
-    <div>WatchPage</div>
+    <div className='px-5'>
+      <iframe
+        width="800"
+        height="400"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen>
+      </iframe>
+    </div>
   )
 }
 
